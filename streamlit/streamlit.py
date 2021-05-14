d6d242c63e2fe8ddd5d635039a9de3789060313e
Leão <heylucasleao@gmail.com>
2021-05-13 20:56:17 -0400

    finalizando projeto

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -3,25 +3,158 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from os import listdir
 
 PATH = r'C:\Users\heylu\Documents\github\A Study Case About Mental Health Tweets in Pandemic Times\streamlit\frequency.csv'
+TREND = r'C:\Users\heylu\Documents\github\A Study Case About Mental Health Tweets in Pandemic Times\streamlit\trend.csv'
+
+def fig_frequency_per_day():
+    fig = px.line(frequency_per_day.rename(columns={'quantidade': 'Tweets'}), 
+    labels={'value': 'Quantidade', 
+    'date': 'Data',
+    'quantidade': 'Tweets'},
+    title='Tweets por Dia')
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:,f}")
+    return fig
+
+def fig_frequency_per_year():
+    frequency_per_year['date'] = [str(x) for x in frequency_per_year['date']]
+    frequency_per_year['date'] = [x[:4] for x in frequency_per_year['date']]
+    fig = px.bar(frequency_per_year, 
+    x='date', 
+    y='quantidade',
+    labels={'quantidade': 'Tweets',
+    'date': 'Date'})
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:,f}")
+    return fig
+
+def fig_frequency_per_month():
+    fig = px.line(frequency_per_month.rename(columns={'quantidade': 'Tweets'}), 
+    labels={'value': 'Quantidade', 'date': 'Data'},
+    title='Tweets por Mês',
+    line_shape='spline')
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:,f}")
+    return fig
+
+def fig_diff_year():
+    diff_per_year['date'] = [str(x) for x in diff_per_year['date']]
+    diff_per_year['date'] = [x[:4] for x in diff_per_year['date']]
+    fig = px.bar(diff_per_year, 
+    x='date', 
+    y='quantidade',
+    labels={'quantidade': 'Percentual (%)',
+    'date': 'Data'},
+    title='Crescimento Anual de Tweets sobre Ansiedade')
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:.2f} %")
+    return fig
+
+def fig_diff_month():
+    diff_per_month.rename(columns={'quantidade': 'Percentual'}, inplace=True)
+    fig = px.bar(diff_per_month.round(2),
+    labels={'value': 'Percentual (%)',
+    'date': 'Data'},
+    title='Crescimento Mensal de Tweets sobre Ansiedade')
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, 
+    hovertemplate="%{y} %")
+    return fig
+
+def fig_total_labels():
+    total_labels = round((frequency['labels'].value_counts() / len(frequency['labels'])) * 100, 2)
+    total_labels = total_labels.reset_index()
+    total_labels.rename(columns={'labels': 'value', 'index': 'labels'}, inplace=True)
+    total_labels['categorias'] = ['Ansiedade (Geral)',
+    'Crise',
+    'Depressão',
+    'Terapia',
+    'Assuntos Graves']
+    fig = px.bar(total_labels, 
+    x='categorias',
+    y='value',
+    labels={'value': 'Percentual (%)', 'date': 'Data', 'categorias': "Categoria"},
+    title='Distribuição de Categorias')
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:2.f} %")
+    return fig
+
+def fig_frequency_per_label():
+    fig = px.line(frequency_per_label, 
+    x='date', 
+    y='quantidade', 
+    facet_row='labels', 
+    height=1200, 
+    width=800,
+    labels={'quantidade': 'Tweets', 'date': 'Data'},
+    title='Frequência por Categoria (Diária)')
+    fig.update_yaxes(matches=None)
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:,f}")
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1], textangle=45))
+    return fig
+
+def fig_frequency_per_label_per_month():
+    df = frequency_per_label.copy()
+    df = df.set_index('date')
+    df.index = pd.to_datetime(df.index)
+    frequency_per_label_per_month = df.groupby(['labels',pd.Grouper(freq='M')]).sum()
+    frequency_per_label_per_month = frequency_per_label_per_month.reset_index()
+
+    fig = px.line(frequency_per_label_per_month, 
+    x='date', 
+    y='quantidade', 
+    facet_row='labels', 
+    labels={'quantidade': 'Tweets', 'date': 'Data'},
+    title='Frequência por Categoria (Mensal)')
+    fig.update_yaxes(matches=None)
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:,f}")
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1], textangle=45))
+    return fig
+
+def fig_trend():
+    trend.rename(columns={'trend': 'Tendência'}, inplace=True)
+    fig = px.line(trend,
+    labels={'value': 'Quantidade Média',
+    'index': 'Data'},
+    title='Predição de Tendência de Tweets')
+    fig.update_layout(hovermode='x', separators=",.")
+    fig.update_traces(showlegend=False, hovertemplate="%{y:,f}")
+    return fig
 
 frequency = pd.read_csv(PATH)
-
+frequency.drop(columns='Unnamed: 0', inplace=True)
 frequency_per_day = frequency.groupby('date').sum()
 frequency_per_day.index = pd.to_datetime(frequency_per_day.index)
 frequency_per_day.index.freq = 'D'
+frequency_per_year = frequency_per_day.groupby(pd.Grouper(freq='Y')).sum()
+frequency_per_year.reset_index(inplace=True)
 frequency_per_month = frequency_per_day.groupby(pd.Grouper(freq='M')).sum()
 frequency_per_month.drop(columns='labels', inplace=True)
 frequency_per_day.drop(columns='labels', inplace=True)
 
-fig_frequency_per_day = px.line(frequency_per_day)
-fig_frequency_per_month = px.line(frequency_per_month)
+frequency_per_label = frequency.groupby(['labels', 'date']).sum()
+frequency_per_label = frequency_per_label.reset_index()
+
+diff_per_year = frequency_per_day[frequency_per_day.index < '2021-01-01'].groupby(pd.Grouper(freq='Y')).sum().pct_change()
+diff_per_year.reset_index(inplace=True)
+diff_per_year['quantidade'] = diff_per_year['quantidade'] * 100
+
+diff_per_month = frequency_per_month.pct_change() * 100
+
+dici = {0: 'Ansiedade (Geral)',
+    1: 'Crise',
+    3: 'Depressão',
+    2: 'Terapia',
+    4: 'Assuntos Graves'}
+frequency_per_label['labels'] = frequency_per_label['labels'].apply(lambda x: dici[x])
+trend = pd.read_csv(TREND, index_col='Unnamed: 0')
 
 st.set_page_config(page_title='Ansiedade e Pandemia')
 
-st.title('Ansiedade e Pandemia: Análise Exploratória no Twitter ao longo de 4 anos.')
+st.title('Ansiedade e Pandemia: Análise Exploratória no Tweets ao longo de 4 anos.')
 
 st.write(f"""Qualquer pesquisa, seja ela, vem por meio de uma curiosidade, um problema ou 
 um interesse para com sociedade. Isto não foi tão diferente deste projeto. Por vezes,
@@ -43,13 +176,20 @@ Como twitter há uma quantidade enorme de dados, resolvi seguir as seguintes reg
     4. Não haveria tweets iguais.
     
 Seguindo estas regras, foi coletado cerca de 7,5 milhões de tweets, na qual podem ser baixados pelo [archive](https://archive.org/download/scraping-em-ansiedade/), um número que considerei atrativo
-para ter noção de magnitude sobre ansiedade. Foram preservados os nomes dos usuários para tal.
+para ter noção de magnitude sobre ansiedade. Foram preservados os nomes dos usuários para tal.""")
 
-{st.plotly_chart(fig_frequency_per_day)}
+box1 = st.selectbox('Selecione período do gráfico',('Diário', 'Mensal', 'Anual'), key=1)
 
-{st.plotly_chart(fig_frequency_per_month)}
+if 'Diário' in box1:
+    st.plotly_chart(fig_frequency_per_day())
+elif 'Mensal' in box1:
+    st.plotly_chart(fig_frequency_per_month())
+elif 'Anual' in box1:
+    st.write("""Se tratando da frequência anual, é observado um aumento bem visual durante os 4 anos. 
+Um detalhe que, apenas 4 meses de 2021 já foram maiores que o ano inteiro de 2019.""")
+    st.plotly_chart(fig_frequency_per_year())
 
-Ainda sim, queria mais respostas, saber se em contextos específicos sobre o tema houve 
+st.write("""Ainda sim, queria mais respostas, saber se em contextos específicos sobre o tema houve 
 maior crescimento. Como 7,5 milhões é um número muito grande para se categorizar manualmente,
 necessitava de um modelo de aprendizagem de máquina para processamento de linguagem natural, 
 e um que vem se destacando atualmente por sua capacidade de extrair contextos 
@@ -68,4 +208,46 @@ Durante o treino, obteve 94% de acurácia nos dados de treino e 83% nos dados de
 os dados de treino, teste e o logger estão dentro do repositório.
 
 Como este estudo tem como objetivo uma dimensão de magnitude, considerei o percentual de acerto suficiente para satisfazer meus desejos.""")
+
+st.plotly_chart(fig_total_labels())
+
+st.write("""Após processamento dos dados, foi identificado que 66,54% falam sobre ansiedade em geral,
+5,61% sobre tratamentos para tal, 20,01% para crises, 6,01% sobre tópicos relacionados a 
+depressão junto a ansiedade e 1,83% sobre tweets relacionados a possíveis assuntos mais 
+graves, como suicídio.""")
+
+box2 = st.selectbox('Selecione período do gráfico',('Diário', 'Mensal'), key=2)
+
+if 'Diário' in box2:
+    st.plotly_chart(fig_frequency_per_label())
+elif 'Mensal' in box2:
+    st.plotly_chart(fig_frequency_per_label_per_month())
+
+st.write("""Ao visualizar a frequência de cada categoria por dia e por mês, observou-se que 
+seus aumentos foram similares e proporcionais, sem identificar um crescimento específico em 
+uma delas em alguma época, o que responde minha dúvida se algum tema teve um pico específico 
+comparado aos outros.""")
+
+box3 = st.selectbox('Selecione período do gráfico',('Anual', 'Mensal'), key=3)
+
+if 'Anual' in box3:
+    st.plotly_chart(fig_diff_year())
+elif 'Mensal' in box3:
+    st.plotly_chart(fig_diff_month())
+
+st.write("""Observando pelo crescimento percentual, vemos que 2019 teve um aumento de 57% 
+sobre tweets, e 100% em 2020 comparado 2019. Dividindo cada crescimento percentual por meses
+a partir de 2018, conseguimos observar uma tendência de aumento ao redor dos meses, com o 
+pico no mês de março de 2020.""")
+
+st.write("""A partir disto, gostaria também saber qual seria a predição para os próximos 
+meses. Para isto, selecionei meu modelo preferido para dados sequencias, o [Light GBM](https://lightgbm.readthedocs.io/en/latest/), 
+e o treinei a partir da frequência por dias. Com hiperparâmetros selecionados por [método Bayesiano](https://scikit-optimize.github.io/stable/modules/generated/skopt.gp_minimize.html), 
+ele atingiu em média 10% de sMAPE na validação cruzada por [Expanding Window Splitter](https://www.sktime.org/en/latest/api_reference/modules/auto_generated/sktime.forecasting.model_selection.ExpandingWindowSplitter.html), um dos
+métodos de validação cruzada para séries temporais. Devido a sazonalidade diária e apenas meu 
+desejo de observar a tendência, suavizei exponencialmente esta predição, por Holt & Winters.
+É possível observar uma possível continuiação de tendência sobre o tema ao redor deste ano, com uma média de 
+tweets por mês chegando a 539 mil.""")
+
+st.plotly_chart(fig_trend())
 
